ed552bc811c703f4c56b53010e7c02bf59d1a998
Dmitriy <easportstng@gmail.com>
2021-08-14 13:17:47 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if(!arr || arr.length == 0) {
+    return 0;
+  }
+  let least = Infinity;
+  arr.forEach( num => {
+      if (num < least) {
+          least = num;
+      }
+  });
+  return least;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if(!arr || arr.length == 0) {
+    return 0;
+  }
+  let biggest = Number.NEGATIVE_INFINITY;
+  arr.forEach( num => {
+      if (num > biggest) {
+          biggest = num;
+      }
+  });
+  return biggest;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if(!arr || arr.length == 0) {
+      return 0;
+  }
+  let total = 0;
+  arr.forEach( num => {
+      total += num;
+  });
+  return total / arr.length;
 }
